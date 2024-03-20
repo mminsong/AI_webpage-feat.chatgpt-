b5d68efdf537622fdd3aeb9a1aaedccac15e0262
mminsong <pamten@naver.com>
2024-03-20 21:55:02 +0900

    1차

---
+++ script.js
@@ -0,0 +1,4 @@
+const generateBtn=document.querySelector('#generateBtn') //html에서 id가 generateBtn인 버튼을 찾아 generateBtn 변수에 저장
+generateBtn.addEventListener('click',()=>{ //generateBtn 버튼에 click이라는 이벤트가 발생하면 console.log를 띄우겠다.
+    console.log('버튼을 누르면 이 함수가 실행?')
+})
